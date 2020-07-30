e2136658c9bde2732594a8a2abadf417e4ccf0b5
jsantos <jsantos.wis@gmail.com>
2020-07-30 12:15:59 -0300

    Add project files.

---
+++ fwlogistic_test/FLCallbackHandler.cs
@@ -0,0 +1,83 @@
+﻿using fwlogistic_test.FWLServiceReference;
+using System;
+
+namespace fwlogistic_test
+{
+    class FLCallbackHandler : fwlsServiceCallback
+    {
+        public IAsyncResult BeginErrorInProblem(ProblemAbstract p, string errorMessage, AsyncCallback callback, object asyncState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncResult BeginProblemCanceled(ProblemAbstract p, AsyncCallback callback, object asyncState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncResult BeginProblemEvaluated(ProblemAbstract p, AsyncCallback callback, object asyncState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncResult BeginProblemSolved(ProblemAbstract p, AsyncCallback callback, object asyncState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncResult BeginServerPing(DateTime timestamp, AsyncCallback callback, object asyncState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndErrorInProblem(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndProblemCanceled(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndProblemEvaluated(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndProblemSolved(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndServerPing(IAsyncResult result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorInProblem(ProblemAbstract p, string errorMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProblemCanceled(ProblemAbstract p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProblemEvaluated(ProblemAbstract p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProblemSolved(ProblemAbstract p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ServerPing(DateTime timestamp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ fwlogistic_test/Program.cs
@@ -0,0 +1,85 @@
+﻿using fwlogistic_test.FWLServiceReference;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace fwlogistic_test
+{
+    public class Program
+    {
+        private static FLCallbackHandler _callbacks;
+        private static InstanceContext context;
+        private static fwlsServiceClient _client;
+        private static fwlsSession _session = null;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio");
+
+            try
+            {
+                string err_msg = null;
+                _callbacks = new FLCallbackHandler();
+                context = new InstanceContext(_callbacks);
+                _client = new fwlsServiceClient(context);
+
+                Console.WriteLine("Ejecutando: SessionCreate");
+                var _session = _client.SessionCreate(out err_msg, "WIS", "wis", "wis");
+
+                if (string.IsNullOrEmpty(err_msg) && _session != null)
+                {
+                    Console.WriteLine("SessionCreate: OK");
+
+                    int value = _client.ServerAlive();
+
+                    Coords coords = new Coords
+                    {
+                        x = -3.695736,
+                        y = 40.426104
+                    };
+
+                    List<Coords> coordsList = new List<Coords>
+                    {
+                                coords
+                    };
+
+                    LocateData locateData = new LocateData
+                    {
+                        coords = coordsList,
+                        maxHierarchy = 0,
+                        maxSnapTolerance = 2500
+                    };
+
+                    Console.WriteLine("Ejecutando LocateOverNetworkAsync");
+                    _client.LocateOverNetworkAsync(_session, new Guid(), locateData, null);
+                }
+                else
+                {
+                    Console.WriteLine("SessionCreate ERROR: " + err_msg);
+                }
+
+                Console.WriteLine("Service Wait");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("EXCEPTION");
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                if (_session != null && _client != null)
+                {
+                    string errmsg = null;
+                    _client.SessionClose(out errmsg, _session);
+                    if (!string.IsNullOrEmpty(errmsg))
+                    {
+                        Console.WriteLine("ERROR SessionClose: " + errmsg);
+                    }
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para terminar");
+            string input_fin = Console.ReadLine();
+        }
+    }
+}
